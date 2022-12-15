b5967e3ebd14e16c505381d5c68cf22fd0d1c4f7
Andrey Zherebor <andreizherebor@yandex.ru>
2022-12-15 11:23:59 +0700

    исправления ошибок после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,30 @@
-let popup = document.querySelector('.popup');
-let popupForm = popup.querySelector('.popup__content');
-let openPopupButton = document.querySelector('.profile__edit-button');
-let closePopupButton = document.querySelector('.popup__close-button');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let inputName = document.querySelector('[name="profileName"]');
-let inputText = document.querySelector('[name="profileText"]');
+const popup = document.querySelector('.popup');
+const popupForm = popup.querySelector('.popup__content');
+const popupOpen = document.querySelector('.profile__edit-button');
+const popupClose = document.querySelector('.popup__close-button');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const nameInput = document.querySelector('.popup__input_type_name');
+const textInput = document.querySelector('.popup__input_type_text');
 
 function openPopup() {
   popup.classList.add('popup_opened');
-  inputName.value = profileTitle.textContent;
-  inputText.value = profileSubtitle.textContent;
+  nameInput.value = profileTitle.textContent;
+  textInput.value = profileSubtitle.textContent;
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+function handlerFormSubmit(evt) {
   evt.preventDefault();
-  profileTitle.textContent = inputName.value;
-  profileSubtitle.textContent = inputText.value;
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = textInput.value;
   closePopup();
 }
 
-function closePopupBackground (evt) {
-  evt.stopPropagation();
-}
 
-popup.addEventListener('click', closePopup);
-popupForm.addEventListener('click', closePopupBackground);
-popupForm.addEventListener('submit', formSubmitHandler);
-openPopupButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', handlerFormSubmit);
+popupOpen.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
